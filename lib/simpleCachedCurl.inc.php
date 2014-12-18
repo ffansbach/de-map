807044fc8b24d96e5f6d76c85e5247495491c552
Tino Dietel <tino.dietel@projekt2k.de>
2014-12-18 12:50:06 +0100

    lib/simpleCachedCurl: added handling for redirects
    allow redirects and handle redirects if savemode is on

--- lib/simpleCachedCurl.inc.php
+++ lib/simpleCachedCurl.inc.php
@@ -24,60 +24,116 @@ call this function with 3 parameters:
 returns either the raw cURL data or false if request fails and no cache is available
 
 */
-function simpleCachedCurl($url,$expires,$debug=false){
-    if($debug){
-        echo "simpleCachedCurl debug:<br>";
+function simpleCachedCurl($url,$expires,$debug=false)
+{
+    if($debug)
+    {
+        echo "simpleCachedCurl debug:<br>\n";
+        echo "$url<br>\n";
     }
+
     $hash = md5($url);
     $filename = dirname(__FILE__).'/../cache/' . $hash . '.cache';
     $changed = file_exists($filename) ? filemtime($filename) : 0;
     $now = time();
     $diff = $now - $changed;   
-    if ( !$changed || ($diff > $expires) ) {
-        if($debug){
-            echo "no cache or expired --> make new request<br>";
+
+    if ( !$changed || ($diff > $expires) )
+    {
+        if($debug)
+        {
+            echo "no cache or expired --> make new request<br>\n";
         }
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_VERSION_IPV6);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 2);
         curl_setopt($ch, CURLOPT_USERAGENT,'php parser for http://de-map.freifunk-emskirchen.de/');
         $rawData = curl_exec($ch);
+
+        $status = curl_getinfo($ch);
+
+        if($status['http_code'] == 301 || $status['http_code'] == 302)
+        {
+            $headerData = _curlGetHeader($url);
+            list($header) = explode("\r\n\r\n", $headerData, 2);
+            $matches = array();
+            preg_match("/(Location:|URI:)[^(\n)]*/", $header, $matches);
+            $url = trim(str_replace($matches[1],"",$matches[0]));
+            $url_parsed = parse_url($url);
+            return (isset($url_parsed))? simpleCachedCurl($url, $expires, $debug):'';
+        }
+
         curl_close($ch);
-        if(!$rawData){
-            if($debug){
-                echo "cURL request failed<br>";
+
+        if(!$rawData)
+        {
+            if($debug)
+            {
+                echo "cURL request failed<br>\n";
             }
-            if($changed){
-                if($debug){
-                    echo "at least we have an expired cache --> better than nothing --> read it<br>";
+
+            if($changed)
+            {
+                if($debug)
+                {
+                    echo "at least we have an expired cache --> better than nothing --> read it<br>\n";
                 }
+
                 $cache = unserialize(file_get_contents($filename));
                 return $cache;
-            }else{
-                if($debug){
-                    echo "request failed and we have no cache at all --> FAIL<br>";
+            }
+            else
+            {
+                if($debug)
+                {
+                    echo "request failed and we have no cache at all --> FAIL<br>\n";
                     echo 'Curl-Fehler: ' . curl_error($ch);
                 }
 
                 return false;
             }
         }
-        if($debug){
-            echo "we got a return --> save it to cache<br>";
+
+        if($debug)
+        {
+            echo "we got a return --> save it to cache<br>\n";
         }
+
         $cache = fopen($filename, 'wb');
         $write = fwrite($cache, serialize($rawData));
-        if($debug && !$write){
+
+        if($debug && !$write)
+        {
             echo "writing to $filename failed. Make the folder '".dirname(__FILE__).'/../cache/'."' is writeable (chmod 777)<br>";
         }
+
         fclose($cache);
         return $rawData;
     }
-    if($debug){
-        echo "yay we hit the cache --> read it<br>";
+
+    if($debug)
+    {
+        echo "yay we hit the cache --> read it<br>\n";
     }
+
     $cache = unserialize(file_get_contents($filename));
     return $cache;
 }
-?>
+
+function _curlGetHeader($url)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_NOBODY, true);
+    curl_setopt($curl, CURLOPT_HEADER, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $header = curl_exec($curl);
+    curl_close($curl);
+    return $header;
+}
