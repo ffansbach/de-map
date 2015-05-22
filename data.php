893898757ef3c4316738a1b8faa91da015d54939
StilgarBF <tino.dietel@projekt2k.de>
2015-05-22 08:44:55 +0200

    data.php: refine client-side caching

--- data.php
+++ data.php
@@ -4,8 +4,8 @@
  *
  * this will try to load a cched result if not older than 24h
  */
-header('Cache-Control: no-cache, must-revalidate');
 $offset = 1 * 60 * 60;
+header('Cache-Control: public, max-age='.$offset);
 header ("Expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT");
 header('Content-Type: application/json');
 error_reporting(-1);
