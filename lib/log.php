fd6cc8d71668c5ec9f37dc5e56ba761bb038e69a
Tino Dietel <tino.dietel@projekt2k.de>
2020-12-28 07:14:58 +0100

    cleanup lib/log

--- lib/log.php
+++ lib/log.php
@@ -1,46 +1,65 @@
 <?php
+namespace ffmap;
+
 class log
 {
-	private $_db;
+    /**
+     * @var mysqli
+     */
+    protected mysqli $db;
 
-	public function __construct($db)
-	{
-		$this->_db = $db;
-	}
+    /**
+     * log constructor.
+     * @param mysqli$db
+     */
+    public function __construct(mysqli $db)
+    {
+        $this->db = $db;
+    }
 
-	public function add($count)
-	{
-		$query = "INSERT INTO log SET `count` = ".(int)$count;
-		$this->_db->query($query);
-	}
+    /**
+     * @param int $count
+     */
+    public function add(int $count)
+    {
+        $query = "INSERT INTO log SET `count` = ".(int)$count;
+        $this->db->query($query);
+    }
 
-	public function get()
-	{
-		$query = "SELECT
-					TS,
-					MIN(count) AS daymin,
-					MAX(count) AS daymax
-				FROM
-					`log`
-				WHERE
-					`count` > 5000
-				GROUP BY DATE(`ts`)
-				ORDER BY ts";
-		$result = $this->_db->query($query);
+    /**
+     * @return array[]
+     */
+    public function get(): array
+    {
+        $query = "SELECT
+                    TS,
+                    MIN(count) AS daymin,
+                    MAX(count) AS daymax
+                FROM
+                    `log`
+                WHERE
+                    `count` > 5000
+                GROUP BY DATE(`ts`)
+                ORDER BY ts";
+        $result = $this->db->query($query);
 
-		$resultArray = array();
+        $resultArray = array();
+        $lastMin = 0;
 
-		while($row = mysqli_fetch_assoc($result))
-		{
-			$date = date_create((string)$row['TS']);
-			$ts = $date->getTimestamp();
-			$resultArray[] = array(
-				'ts' => $ts,
-				'max' => $row['daymax'],
-				'min' => $row['daymin']
-			);
-		}
+        while ($row = mysqli_fetch_assoc($result)) {
+            $date = date_create((string)$row['TS']);
+            $ts = $date->getTimestamp();
 
-		return $resultArray;
-	}
+            if ($row['daymin'] > ($lastMin * 0.75)) {
+                $resultArray[] = array(
+                    'ts' => $ts,
+                    'max' => $row['daymax'],
+                    'min' => $row['daymin']
+                );
+            }
+            $lastMin = $row['daymin'];
+        }
+
+        return $resultArray;
+    }
 }
