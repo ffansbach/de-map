c5e3c1b76ba7584a2c944652c2e919b5fe5b4f41
Tino Dietel <tino.dietel@projekt2k.de>
2020-12-29 07:30:31 +0100

    upload_cache - direct to live server
    unfortunately that one only has ftp atm. so rewrote the upload to ftp.

--- upload_cache.php
+++ upload_cache.php
@@ -1,47 +1,63 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 require_once 'config.php';
-
-use phpseclib\Net\SFTP;
 
 if (empty($serverUpload)) {
     return;
 }
+echo "\n**************************\n";
+echo "starting result upload\n";
 
-$sftp = new SFTP($serverUpload['host']);
+// ftp connect
+$conn_id = ftp_connect($serverUpload['host']);
+$login_result = ftp_login($conn_id, $serverUpload['user'], $serverUpload['password']);
+ftp_pasv($conn_id, true);
 
-if (!$sftp->login($serverUpload['user'], $serverUpload['password'])) {
-    exit('Login Failed');
+if ((!$conn_id) || (!$login_result)) {
+    exit('FTP login failed');
 }
 
-$sftp->chdir($serverUpload['target']);
 $files = ['result_communities.json', 'result_routers.json', 'result_statistics.json'];
 
+// uploading all files as filename.json.staging
 foreach ($files as $fileName) {
-    print_r('uploading: '.$fileName."\n");
-    $sftp->put(
-        $fileName.'.staging',
-        'cache/'.$fileName,
-        SFTP::SOURCE_LOCAL_FILE
-    );
-    print_r('upload done'."\n");
-}
+    echo 'uploading: '.$fileName."\n";
+    $fp = fopen(__DIR__.'/cache/'.$fileName, 'r');
 
-foreach ($files as $fileName) {
-    print_r('renaming: "'.$fileName.'.staging" to "'.$fileName.'"'."\n");
-
-    if (!$sftp->delete($fileName)) {
-        print_r('delete failed for: "'.$fileName."\n");
-        continue;
+    if (ftp_fput($conn_id, $fileName.'.staging', $fp, FTP_TEXT)) {
+        echo "Successfully uploaded $fileName\n";
+    } else {
+        echo "There was a problem while uploading $fileName\n";
     }
 
-    if (!$sftp->rename(
+    echo 'upload done'."\n";
+}
+
+$fileList = ftp_rawlist($conn_id, '/');
+
+// renaming all filename.json.staging to filename.json
+foreach ($files as $fileName) {
+    echo 'renaming: "'.$fileName.'.staging" to "'.$fileName.'"'."\n";
+
+    if (in_array($fileName, $fileList)) {
+        // file already exists - delete the old one before renaming new
+        if (!ftp_delete($fileName)) {
+            echo 'delete failed for: ' . $fileName . "\n";
+            continue;
+        }
+    }
+
+    if (!ftp_rename(
+        $conn_id,
         $fileName.'.staging',
         $fileName
     )) {
-        print_r('rename failed for: "'.$fileName."\n");
+        echo 'rename failed for: '.$fileName."\n";
         continue;
     }
 
-    print_r('rename done'."\n");
+    echo 'rename done'."\n";
 }
+
+// close the connection
+ftp_close($conn_id);
